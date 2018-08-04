d8132a7cf6885a118af03efb1c7014a18e12d10b
tomdaniels <thomsondaniels@hotmail.com>
2018-08-04 17:26:43 +1000

    feat: initial prompts, stored in props

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,15 +14,50 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        // Defaults to the project's folder name if the input is skipped
+        default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description for your component?'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Project author'
+      },
+      {
         type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        name: 'repositoryConfirm',
+        message: 'Is there an existing repository for this project?',
+        default: false
+      }
+    ];
+
+    const repositoryUrlPrompt = [
+      {
+        type: 'input',
+        name: 'repository',
+        message: 'Enter the repository URL'
       }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
+      if (props.repositoryConfirm) {
+        return this.prompt(
+          repositoryUrlPrompt,
+        ).then(repositoryUrl => {
+          const responses = {
+            ...repositoryUrl,
+            ...this.props,
+          };
+        })
+      }
       this.props = props;
     });
   }
